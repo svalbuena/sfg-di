3e2db120b0ad838b34cda5c99ce8a9afc07bf8ee
svalbuena <sergivalbuena@hotmail.com>
2021-03-30 23:02:37 +0200

    Get MyController bean from ApplicationContext

--- src/main/java/svalbuena/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/svalbuena/springframework/sfgdi/SfgDiApplication.java
@@ -2,12 +2,18 @@ package svalbuena.springframework.sfgdi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import svalbuena.springframework.sfgdi.controllers.MyController;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SfgDiApplication.class, args);
+    public static void main(final String[] args) {
+        final ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
+        final MyController myController = (MyController) context.getBean("myController");
+
+        final String greeting = myController.sayHello();
+        System.out.println(greeting);
     }
 
 }
