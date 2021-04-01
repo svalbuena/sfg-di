6d3c53b0b4bab976f92d25ca207614c336dc0f38
svalbuena <sergivalbuena@hotmail.com>
2021-04-01 11:42:12 +0200

    ComponentScan example

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -4,7 +4,10 @@ import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
+@ComponentScan(basePackages = {"guru.springframework.sfgdi", "guru.springframework.pets"})
 @SpringBootApplication
 public class SfgDiApplication {
 
